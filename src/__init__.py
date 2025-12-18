982279e725da1e513d000ca3968339816946cee4
ericcouture <63058212+ericcouture@users.noreply.github.com>
2025-12-18 12:50:48 -0500

    workflow revision

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 """Image Interlacement Program - A Python CLI tool for image interlacing operations."""
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __author__ = "e. couture"
 __description__ = "A simple image interlacement/interleavment program created to aid in creating weave structures for the TC2 loom."
